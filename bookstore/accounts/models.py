f25a5b3681dcd73fff2531fec21bc7c3376b5bdf
johnpauljpc <jpcwork081@gmail.com>
2023-06-21 19:01:31 +0100

    .

--- bookstore/accounts/models.py
+++ bookstore/accounts/models.py
@@ -3,6 +3,10 @@ from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 class CustomUser(AbstractUser):
-    email = models.EmailField()
+    email = models.EmailField(blank=False, null= False, unique=True)
+
+
+    # USERNAME_FIELD = 'email' 
+    # REQUIRED_FIELDS =['username']   
     
     
--- bookstore/accounts/tests.py
+++ bookstore/accounts/tests.py
@@ -1,3 +1,30 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
+from .models import CustomUser
 
 # Create your tests here.
+User = get_user_model()
+
+class TestCustomUserModel(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        
+        user = CustomUser.objects.create_user(
+            username = 'john',
+            # email = 'jp@gmail.com',
+            
+        )
+        user.set_password('pass1234')
+
+    def testUserCreation(self):
+        user = CustomUser.objects.get(id=1)
+        
+        self.assertEqual(user.username, 'john')
+        self.assertTrue(user.is_active)
+        self.assertFalse(user.is_superuser)
+
+    # def testCreateSuperUser(self):
+    #     User.objects.create_superuser(
+    #         username =
+    #     )
+
