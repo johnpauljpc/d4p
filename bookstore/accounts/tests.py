0934e1a8858b88ef7bae167a6f9cae3f6656589c
johnpauljpc <jpcwork081@gmail.com>
2023-06-21 19:04:15 +0100

    tests on custom user model

--- bookstore/accounts/tests.py
+++ bookstore/accounts/tests.py
@@ -1,30 +1,30 @@
-from django.test import TestCase
 from django.contrib.auth import get_user_model
-from .models import CustomUser
+from django.test import TestCase
 
-# Create your tests here.
-User = get_user_model()
-
-class TestCustomUserModel(TestCase):
-    @classmethod
-    def setUpTestData(cls):
-        
-        user = CustomUser.objects.create_user(
-            username = 'john',
-            # email = 'jp@gmail.com',
-            
+class CustomUserTests(TestCase):
+    def test_create_user(self):
+        User = get_user_model()
+        user = User.objects.create_user(
+        username='john',
+        email='john@email.com',
+        password='testpass123'
         )
-        user.set_password('pass1234')
-
-    def testUserCreation(self):
-        user = CustomUser.objects.get(id=1)
-        
         self.assertEqual(user.username, 'john')
+        self.assertEqual(user.email, 'john@email.com')
         self.assertTrue(user.is_active)
+        self.assertFalse(user.is_staff)
         self.assertFalse(user.is_superuser)
 
-    # def testCreateSuperUser(self):
-    #     User.objects.create_superuser(
-    #         username =
-    #     )
 
+    def test_create_superuser(self):
+        User = get_user_model()
+        admin_user = User.objects.create_superuser(
+        username='admin',
+        email='admin@email.com',
+        password='testpass123'
+        )
+        self.assertEqual(admin_user.username, 'admin')
+        self.assertEqual(admin_user.email, 'admin@email.com')
+        self.assertTrue(admin_user.is_active)
+        self.assertTrue(admin_user.is_staff)
+        self.assertTrue(admin_user.is_superuser)
