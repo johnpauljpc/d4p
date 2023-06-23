6207b465cfbc40b19a4ffba1a5046777246e7566
johnpauljpc <jpcwork081@gmail.com>
2023-06-23 14:23:51 +0100

    tests for home page url and url_name

--- bookstore/pages/tests.py
+++ bookstore/pages/tests.py
@@ -1,3 +1,15 @@
-from django.test import TestCase
+from django.test import TestCase, SimpleTestCase
+from django.urls import reverse
 
 # Create your tests here.
+class TestHomePage(SimpleTestCase):
+    # @classmethod
+    def testHomepageStatus(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+        print('url tested')
+
+    def test_homepage_url_name(self):
+        response = self.client.get(reverse('home'))
+        self.assertEqual(response.status_code, 200)
+        print('url name tested')
