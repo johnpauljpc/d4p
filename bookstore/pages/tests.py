f86913746c53f09f9f57c731ba06c56fc151f86e
johnpauljpc <jpcwork081@gmail.com>
2023-06-23 14:52:54 +0100

    .

--- bookstore/pages/tests.py
+++ bookstore/pages/tests.py
@@ -13,3 +13,11 @@ class TestHomePage(SimpleTestCase):
         response = self.client.get(reverse('home'))
         self.assertEqual(response.status_code, 200)
         print('url name tested')
+
+    def test_homepage_template(self):
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'home.html')
+
+    def test_homepage_contains_correct_html(self):
+        response = self.client.get('/')
+        self.assertContains(response, 'Halo')
