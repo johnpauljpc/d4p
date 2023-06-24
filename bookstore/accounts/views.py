b5ff186118c664b6b525120e0c7eed31d36336da
johnpauljpc <jpcwork081@gmail.com>
2023-06-24 20:01:18 +0100

    .

--- bookstore/accounts/views.py
+++ bookstore/accounts/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import View, CreateView
+
+# imports from local files
+from .forms import CustomUserCreationForm
 
 # Create your views here.
+class SignupView(CreateView):
+    template_name = 'registration/signup.html'
+    form_class = CustomUserCreationForm
+    success_url = reverse_lazy('login')
+# class SignupView(View):
+#     def get(self, request):
+#         form = CustomUserCreationForm()
+
+
+#         return render(request, 'registration/signup.html', context={'form':form})
