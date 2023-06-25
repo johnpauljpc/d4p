93f717a531e2c4833d39c7da105b4cb9fc330f8d
johnpauljpc <jpcwork081@gmail.com>
2023-06-25 23:03:04 +0100

    .

--- bookstore/pages/urls.py
+++ bookstore/pages/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path 
-from .views import home
+from .views import home, AboutUs, ContactUs
 
 
 urlpatterns = [
-    path('', home, name='home')
+    path('', home, name='home'),
+    path('about/', AboutUs.as_view(), name="about"),
+    path('contact-us/', ContactUs.as_view(), name="contact-us")
 ]
--- bookstore/pages/views.py
+++ bookstore/pages/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
+
+class AboutUs(TemplateView):
+    template_name = 'about.html'
+
+
+class ContactUs(TemplateView):
+    template_name = 'contact-us.html'
