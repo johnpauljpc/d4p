8bc9895d5a1035767a12332a783e41f3f0925038
johnpauljpc <jpcwork081@gmail.com>
2023-06-26 18:09:21 +0100

    logout page

--- bookstore/config/urls.py
+++ bookstore/config/urls.py
@@ -16,14 +16,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from allauth.account.views import SignupView
+from allauth.account.views import SignupView, LogoutView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 
     #User management
     # path('accounts/', include('django.contrib.auth.urls')),
-    path('accounts/signup/', SignupView.as_view(template_name = 'registration/signup.html')),
+    path('accounts/signup/', SignupView.as_view(template_name = 'registration/signup.html'), name='account_signup'),
+    path('accounts/logout/', LogoutView.as_view(template_name = 'registration/logout.html'), name='account_logout'),
     path('accounts/', include('allauth.urls')),
 
     #Local apps
