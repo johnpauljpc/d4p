d428d85536ecc6d190ed9d7d541c58554a4c41bd
johnpauljpc <jpcwork081@gmail.com>
2023-06-30 13:11:46 +0100

    urls for books

--- bookstore/books/urls.py
+++ bookstore/books/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path 
+from .views import BookList, BookDetail, EditBook, DeleteBook
 
 
 urlpatterns=[
-    
+    path('', BookList.as_view(), name="book-list" ),
+    path('<int:pk>/detail/', BookDetail.as_view(), name="book-detail" ),
+    path('<int:pk>/edit/', EditBook.as_view(), name="edit-book"),
+    path('<int:pk>/delete/', DeleteBook.as_view(), name="delete-book"),
 ]
--- bookstore/books/views.py
+++ bookstore/books/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
+from django.views.generic import ListView, DetailView, UpdateView, DeleteView
+
 # Create your views here.
+#booklist book detailview CRUD
+class BookList(ListView):
+    pass
+
+class BookDetail(DetailView):
+    pass
+
+class EditBook(UpdateView):
+    pass
+
+class DeleteBook(DeleteView):
+    pass
