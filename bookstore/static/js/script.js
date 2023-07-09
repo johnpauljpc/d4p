e478cf59a1fe8e0c147cdfe6bdf687644c54c585
johnpauljpc <jpcwork081@gmail.com>
2023-07-09 21:08:12 +0100

    .

--- bookstore/static/js/script.js
+++ bookstore/static/js/script.js
@@ -1,6 +1,20 @@
-review = document.querySelector("#review")
+// review = document.querySelector("#review")
 
-review.addEventListener('submit', (e)=> {
+// review.addEventListener('submit', (e)=> {
     
-    console.log("hiiiiiiiiiiii")
-})
+//     console.log("hiiiiiiiiiiii")
+// })
+
+
+// search = document.querySelector("#search")
+// searchBtn = document.querySelector("#search-btn")
+
+// search.addEventListener('keyup', (e)=> {
+//     searchVal = e.target.value;
+//     if (searchVal.trim().length < 1){
+        
+//     }
+//     else{
+        
+//     }
+// })
