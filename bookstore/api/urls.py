cc876d72154eed3f82e319cd0b79f280bd54b441
johnpauljpc <jpcwork081@gmail.com>
2023-07-17 22:21:48 +0100

    .

--- bookstore/api/urls.py
+++ bookstore/api/urls.py
@@ -1,7 +1,33 @@
 from django.urls import path
 from .views import Home, BookRUD
 
+from drf_yasg.views import get_schema_view as gsv
+from drf_yasg import openapi
+from rest_framework import permissions
+
+
+schema_view = gsv( # new
+openapi.Info(
+title="Blog API",
+default_version="v1",
+description="API endpoints for bookstore",
+terms_of_service="https://www.google.com/policies/terms/",
+contact=openapi.Contact(email="contact@blogapi.com"),
+license=openapi.License(name="BSD License"),
+),
+public=True,
+permission_classes=(permissions.AllowAny,),
+)
+
+
 urlpatterns = [
     path('', Home.as_view(), name='home-api' ),
     path('book/<uuid:pk>/', BookRUD.as_view(), name = "book-rud"),
+
+
+
+    # Endpoints as a result of drf_yasg
+    path('swagger/', schema_view.with_ui( # new
+        'swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
--- bookstore/config/settings.py
+++ bookstore/config/settings.py
@@ -54,6 +54,7 @@ INSTALLED_APPS = [
 
     #for api
     'rest_framework',
+    'drf_yasg',
 
     # Local apps
     'accounts',
